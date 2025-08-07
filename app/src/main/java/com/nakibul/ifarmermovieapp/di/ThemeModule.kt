a71fbb68d7d996d83c17b15c7a9fb1d6b4ca4b4d
nakibul11426 <nakibul.hassan@bjitgroup.com>
2025-08-07 09:34:09 +0600

    - create separate module for theme

---
+++ app/src/main/java/com/nakibul/ifarmermovieapp/di/ThemeModule.kt
@@ -0,0 +1,23 @@
+package com.nakibul.ifarmermovieapp.di
+
+import android.content.Context
+import com.nakibul.ifarmermovieapp.domain.repository.UserPreferencesRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ThemeModule {
+    
+    @Provides
+    @Singleton
+    fun provideUserPreferencesRepository(
+        @ApplicationContext context: Context
+    ): UserPreferencesRepository {
+        return UserPreferencesRepository(context)
+    }
+}
--- app/src/main/java/com/nakibul/ifarmermovieapp/domain/repository/UserPreferencesRepository.kt
+++ app/src/main/java/com/nakibul/ifarmermovieapp/domain/repository/UserPreferencesRepository.kt
@@ -7,16 +7,12 @@ import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.preferencesDataStore
 import com.nakibul.ifarmermovieapp.utils.Constant.USER_PREFERENCE_NAME
-import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
-import javax.inject.Inject
-import javax.inject.Singleton
 private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = USER_PREFERENCE_NAME)
 
-@Singleton
-class UserPreferencesRepository @Inject constructor(
-    @ApplicationContext private val context: Context
+class UserPreferencesRepository(
+    private val context: Context
 ) {
     private val dataStore = context.dataStore
 
